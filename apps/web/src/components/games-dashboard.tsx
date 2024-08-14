6d4b65cb75d55833486c2e795cb879991cc0bc7c
Richard Freitas <wesleyrichard30@gmail.com>
2024-08-14 07:36:48 -0300

    feat: add games table component in dashboard

---
+++ apps/web/src/components/games-dashboard.tsx
@@ -0,0 +1,176 @@
+'use client';
+
+import { IGame } from '@/dtos/game';
+import { useAuth } from '@/hooks/use-auth';
+import { api } from '@/lib/api';
+import { AxiosError } from 'axios';
+import dayjs from 'dayjs';
+import { ExternalLink, MoreHorizontal, Plus, SquarePen, Trash } from 'lucide-react';
+import NextLink from 'next/link';
+import { useCallback, useEffect, useState } from 'react';
+import { Link } from './ui/link';
+import { Loading } from './ui/loading';
+import {
+  PopoverMenu,
+  PopoverMenuContent,
+  PopoverMenuItem,
+  PopoverMenuTrigger,
+} from './ui/popover-menu';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './ui/table';
+
+export const GamesDashboard = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [games, setGames] = useState<IGame[]>([]);
+
+  const { isLoadingUserData } = useAuth();
+
+  const fetchGames = useCallback(async () => {
+    try {
+      const response = await api.get<{ games: IGame[] }>('/games');
+
+      setGames(response.data.games);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const errorMessage: string | [] = error.response?.data.error;
+
+        console.log(errorMessage);
+      }
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoadingUserData && isLoading) {
+      fetchGames();
+    }
+  }, [fetchGames, isLoading, isLoadingUserData]);
+
+  const handleDeleteGame = async (id: string) => {
+    const action = confirm('Deseja realmente excluir este jogo?');
+
+    if (!action) return;
+
+    setIsLoading(false);
+
+    try {
+      await api.delete(`/games/${id}`);
+
+      await fetchGames();
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const errorMessage: string | [] = error.response?.data.error;
+
+        console.log(errorMessage);
+      }
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <main className="mx-auto flex size-full max-w-[1400px] flex-1 flex-col space-y-5 px-6 py-8">
+      <div className="flex items-center gap-3">
+        <h1 className="text-2xl font-bold">JOGOS</h1>
+
+        <hr className="bg-border h-5 w-0.5" />
+
+        <Link href="/dashboard/games/editor" variant="primary" className="pr-4">
+          <Plus className="size-4" />
+          Novo jogo
+        </Link>
+      </div>
+
+      {isLoading ? (
+        <div className="flex h-full">
+          <Loading />
+        </div>
+      ) : (
+        <div className="overflow-x-auto">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Jogo</TableHead>
+                <TableHead>Lançamento</TableHead>
+                <TableHead>Preço</TableHead>
+                <TableHead>Disponibilidade</TableHead>
+                <TableHead>Gênero</TableHead>
+                <TableHead>Criado em</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {games?.map((game) => {
+                return (
+                  <TableRow key={game.id}>
+                    <TableCell>
+                      <div className="flex flex-col">
+                        <span className="font-medium">{game.title}</span>
+                        <span className="text-xs text-gray-500">{game.id}</span>
+                      </div>
+                    </TableCell>
+                    <TableCell>{game.year}</TableCell>
+                    <TableCell>
+                      {Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                      }).format(game.price)}
+                    </TableCell>
+                    <TableCell>
+                      <span
+                        className="rounded-full bg-yellow-950/70 px-2 py-1 text-yellow-400 data-[public=true]:bg-emerald-950/70 data-[public=true]:text-emerald-400"
+                        data-public={game.disponibility}
+                      >
+                        {game.disponibility ? 'DISPONÍVEL' : 'INDISPONÍVEL'}
+                      </span>
+                    </TableCell>
+                    <TableCell>{game.genre?.name}</TableCell>
+                    <TableCell>{dayjs(game.createdAt).format('DD/MM/YYYY')}</TableCell>
+                    <TableCell>
+                      <PopoverMenu>
+                        <PopoverMenuTrigger>
+                          <MoreHorizontal className="size-4" />
+                        </PopoverMenuTrigger>
+
+                        <PopoverMenuContent>
+                          <PopoverMenuItem asChild>
+                            <NextLink href={`/dashboard/games/${game.slug}`}>
+                              <ExternalLink className="size-4" />
+                              Visualizar
+                            </NextLink>
+                          </PopoverMenuItem>
+
+                          <PopoverMenuItem asChild>
+                            <NextLink href={`/dashboard/posts/editor/${game.id}`}>
+                              <SquarePen className="size-4" />
+                              Editar
+                            </NextLink>
+                          </PopoverMenuItem>
+
+                          <PopoverMenuItem
+                            className="text-destructive"
+                            onClick={() => handleDeleteGame(game.id)}
+                          >
+                            <Trash className="size-4" />
+                            Excluir
+                          </PopoverMenuItem>
+                        </PopoverMenuContent>
+                      </PopoverMenu>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      )}
+    </main>
+  );
+};
---
+++ apps/web/src/dtos/game.ts
@@ -0,0 +1,17 @@
+export interface IGame {
+  id: string;
+  title: string;
+  slug: string;
+  year: number;
+  price: number;
+  imageUrl: string;
+  description: string;
+  disponibility: boolean;
+  createdAt: Date;
+  genreId: string;
+  genre?: {
+    id: string;
+    name: string;
+    createdAt: Date;
+  };
+}
